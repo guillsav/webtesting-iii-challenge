e03fa177b5374aa9a517e1081598ac8d72672b41
guillsav <guillsav@gmail.com>
2019-05-15 15:35:46 -0400

    Initial commit & basic tests on Dashboard done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,51 @@
 // Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import {render, fireEvent} from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+
+describe('<Dashboard />', () => {
+  it('Should render sucessfully', () => {
+    const {getByText} = render(<Dashboard />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+  });
+
+  it('Should close the gate', () => {
+    const {getByText} = render(<Dashboard />);
+    const btnCloseGate = getByText(/close gate/i);
+
+    fireEvent.click(btnCloseGate);
+    getByText(/closed/i);
+  });
+
+  it('Should lock the gate', () => {
+    const {getByText} = render(<Dashboard />);
+
+    const btnCloseGate = getByText(/^close gate$/i);
+    fireEvent.click(btnCloseGate);
+    getByText(/^closed$/i);
+
+    const btnLockGate = getByText(/^lock gate$/i);
+    fireEvent.click(btnLockGate);
+    getByText(/^locked$/i);
+  });
+
+  it('Should unlock the gate', () => {
+    const {getByText} = render(<Dashboard />);
+
+    const btnCloseGate = getByText(/^close gate$/i);
+    fireEvent.click(btnCloseGate);
+    getByText(/^closed$/i);
+
+    const btnLockGate = getByText(/^lock gate$/i);
+    fireEvent.click(btnLockGate);
+    getByText(/^locked$/i);
+
+    const btnUnlockGate = getByText(/^unlock gate$/i);
+    fireEvent.click(btnUnlockGate);
+    getByText(/^unlocked$/i);
+  });
+});
